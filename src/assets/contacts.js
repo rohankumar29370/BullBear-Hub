0c5bc02a6502ced666bc44bb41b66849fda4265d
Taraq Pradhumna Kosaraju <pradhyumna.kt@gmail.com>
2025-03-23 10:54:49 -0500

    Modified JSX code for About and AboutUsCard, added mapping function

---
+++ src/assets/contacts.js
@@ -0,0 +1,38 @@
+import usrimg from './defaultUserpic.jpg';
+
+const contacts = [
+    {
+        id : 1,
+        imageSrc: usrimg,
+        cardTitle : "Tuan Vo", 
+        cardText : "Lead Developer with expertise in AI and blockchain technology.",
+        buttonText : "Learn More",
+        buttonLink: "https://www.linkedin.com"
+    }, 
+    {
+        id :2,
+        imageSrc: usrimg,
+        cardTitle:"Fnu Rohan", 
+        cardText : "Financial Analyst with a strong background in crypto markets." ,
+        buttonText :"Learn More",
+         buttonLink : "https://www.linkedin.com",
+    },
+    {
+        id :3,
+        imageSrc: usrimg,
+        cardTitle : "Taraq Pradhumna Kosaraju", 
+        cardText : "Full Stack Developer driven by a passion for AI and building smart, scalable web applications. Skilled in both frontend and backend technologies, Taraq brings innovative thinking and technical expertise to every project.",
+         buttonText : "Learn More",
+          buttonLink : "https://www.linkedin.com"
+    },
+    {
+        id :4,
+        imageSrc: usrimg,
+        cardTitle : "Jovita Perpetual Mendonca", 
+        cardText : "Innovative strategist with expertise in financial modeling and risk assessment.",
+         buttonText : "Learn More",
+          buttonLink : "https://www.linkedin.com"
+    }
+];
+
+export default contacts;
